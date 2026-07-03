f8568f43c3bd88350b9734c499f23d13ecf99577
jcolange <jade.colanges@live.fr>
2026-07-03 16:18:47 +0200

    Ajout des repository pour User et Product (vides)

---
+++ src/main/java/com/project/lets_play/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.project.lets_play.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.project.lets_play.model.Product;;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+    
+}
