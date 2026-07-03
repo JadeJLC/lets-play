60617b1d6954e13449127929e79523c4ad05f6fe
jcolange <jade.colanges@live.fr>
2026-07-03 18:21:37 +0200

    Ajout temporaire de PasswordEncoder dans app test

--- src/test/java/com/project/lets_play/LetsPlayApplicationTests.java
+++ src/test/java/com/project/lets_play/LetsPlayApplicationTests.java
@@ -2,6 +2,8 @@ package com.project.lets_play;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootTest
 class LetsPlayApplicationTests {
@@ -10,4 +12,10 @@ class LetsPlayApplicationTests {
 	void contextLoads() {
 	}
 
+	public PasswordEncoder createPasswordEncoder() {
+		PasswordEncoder passwordEncoder =
+		PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		return passwordEncoder;
+	}
 }
+
