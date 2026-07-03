3be830c40a8aea9e0ffe6ae33fb55bfb771951ed
jcolange <jade.colanges@live.fr>
2026-07-03 17:34:42 +0200

    Création de UserController
    - constructeur + createUser

---
+++ src/main/java/com/project/lets_play/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.project.lets_play.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.project.lets_play.service.UserService;
+import com.project.lets_play.model.User;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    
+
+}
